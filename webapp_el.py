0d0f854855a64f57a5b93ac3209b5b63f9d49f65
Tob <121010757+tobzas@users.noreply.github.com>
2024-09-19 11:13:22 +0700

    Update webapp_el.py

--- webapp_el.py
+++ webapp_el.py
@@ -3,6 +3,12 @@ import streamlit as st
 st.title("Lecture VDO Selection")
 
 
+title_writing = "Test Title"
+title_format = f'<p style="text-align: center; font-family: ' \
+               f'Arial; color: #808080; font-size: 40px; ' \
+               f'font-weight: bold;">{title_writing}</p>'
+st.markdown(title_format, unsafe_allow_html=True)
+
 
 st.write("This is some text")
 

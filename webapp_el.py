f68e29d9fd05bbc173ce3bf1580efc8b13616d06
Tob <121010757+tobzas@users.noreply.github.com>
2024-09-19 11:01:58 +0700

    Update webapp_el.py

--- webapp_el.py
+++ webapp_el.py
@@ -9,3 +9,8 @@ st.header("This is a header")
 st.markdown("## This an h2 header")
 
 st.caption("This is a caption")
+
+
+import streamlit as st
+url = 'https://www.youtube.com/watch?v=tDt7X6coxvs 39’
+st.video(url)
